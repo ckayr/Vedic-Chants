ee1d4e89167a7302c903d024873f94d9221fd8ca
ckayr <karthik@madras2madurai.com>
2021-12-04 17:24:47 -0800

    Add preliminary version with tamil script

--- stotrANi.sh
+++ stotrANi.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-wkhtmltopdf toc --xsl-style-sheet stotrANi.xsl  stotrANi.html stotrANi.pdf
-wkhtmltopdf toc --xsl-style-sheet stotrANi-mine.xsl  stotrANi.html stotrANi-mine.pdf
+if [ "${1}" == "s" ]; then
+    wkhtmltopdf toc --xsl-style-sheet stotrANi.xsl  stotrANi.html stotrANi.pdf
+    wkhtmltopdf toc --xsl-style-sheet stotrANi-mine.xsl  stotrANi.html stotrANi-mine.pdf
+elif [ "${1}" == "t" ]; then
+    wkhtmltopdf toc --xsl-style-sheet stotrANi-tamil.xsl  stotrANi-tamil.html stotrANi-tamil.pdf
+else 
+    wkhtmltopdf toc --xsl-style-sheet stotrANi.xsl  stotrANi.html stotrANi.pdf
+    wkhtmltopdf toc --xsl-style-sheet stotrANi-mine.xsl  stotrANi.html stotrANi-mine.pdf
+    wkhtmltopdf toc --xsl-style-sheet stotrANi-tamil.xsl  stotrANi-tamil.html stotrANi-tamil.pdf   
+fi
